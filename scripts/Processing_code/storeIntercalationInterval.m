044a9b239debd4d47ce442dffbe23fe70c0d1b2f
Kevin Moon <kevimoon@gmail.com>
2017-06-05 13:44:30 -0400

    Delete storeIntercalationInterval.m

--- scripts/Processing_code/storeIntercalationInterval.m
+++
@@ -1,8 +0,0 @@
-function storeIntercalationInterval(dummy, interval, dataSet)
-    indices = interval.Axes.Children(2).BrushHandles.Children(1).VertexIndices;
-    xMax = max(interval.Axes.Children(2).BrushHandles.Children(1).VertexData(1, indices));
-    xMin = min(interval.Axes.Children(2).BrushHandles.Children(1).VertexData(1, indices));
-    yMax = max(interval.Axes.Children(2).BrushHandles.Children(1).VertexData(2, indices));
-    yMin = min(interval.Axes.Children(2).BrushHandles.Children(1).VertexData(2, indices));
-    dlmwrite(strcat('IntercalationIntervals/', dataSet, '.txt'), [xMin xMax; yMin yMax]);
-end
