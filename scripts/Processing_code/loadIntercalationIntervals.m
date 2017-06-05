42ab3f7cfb1b9fc9ce3c3548181beac7ad983b7d
Kevin Moon <kevimoon@gmail.com>
2017-06-05 13:43:36 -0400

    Delete loadIntercalationIntervals.m

--- scripts/Processing_code/loadIntercalationIntervals.m
+++
@@ -1,7 +0,0 @@
-function [ intercalationIntervals ] = loadIntercalationIntervals(keys)
-    intercalationIntervals = [];
-    for i = 1:size(keys, 2)
-        M = dlmread(strcat('IntercalationIntervals/', keys{i}, '.txt'));
-        intercalationIntervals(:, :, i) = M;
-    end
-end
